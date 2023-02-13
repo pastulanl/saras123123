f087cf4f7eba2a5ebd61049b22e2f221d933911b
Roger <114060033+pastulanl@users.noreply.github.com>
2023-02-13 09:21:34 +0200

    Update bisection.py

--- bisection.py
+++ bisection.py
@@ -30,21 +30,7 @@ def bisection(function: Callable[[float], float], a: float, b: float) -> float:
     ):  # if none of these are root and they are both positive or negative,
         # then this algorithm can't find the root
         raise ValueError("could not find root in given interval.")
-    else:
-        mid: float = start + (end - start) / 2.0
-        while abs(start - mid) > 10**-7:  # until precisely equals to 10^-7
-            if function(mid) == 0:
-                return mid
-            elif function(mid) * function(start) < 0:
-                end = mid
-            else:
-                start = mid
-            mid = start + (end - start) / 2.0
-        return mid
 
-
-def f(x: float) -> float:
-    return x**3 - 2 * x - 5
 
 
 if __name__ == "__main__":
